fcfbf7fe0ac155a9bc91d217b0c721bd31b9f46f
Deciare <deciare@isisview.org>
2016-04-14 05:53:38 -0400

    Proof-of-concept login page

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,56 @@
 	Please see LICENSE for more info
  */
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var lscrypto = require("./../lib/lscrypto");
+var security = require("./../lib/security");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'LifeStream' });
+router.get("/", function(req, res, next) {
+	res.render("index", {
+		title: "LifeStream"
+	});
+});
+router.get("/login", function(req, res, next) {
+	var templateVars = {
+		message: null,
+		title: "LifeStream - Sign in"
+	};
+
+	if (req.query.reason) {
+		switch (req.query.reason) {
+			case "failed_login":
+				templateVars.message = {
+					type: "danger",
+					text: req.query.detail
+				}
+				break;
+			case "session_timeout":
+				templateVars.message = {
+					type: "danger",
+					text: "Your session has expired. To continue using LifeStream, you will need to sign in again."
+				}
+				break;
+			case "successful_login":
+				templateVars.message = {
+					type: "success",
+					text: "You have logged in successfully."
+				}
+		}
+
+		// Redirect user to the URL they were trying to access before they
+		// were redirected to the login page.
+		if (req.query.from) {
+			templateVars.fromUrl = req.query.from;
+		}
+	}
+
+	res.render("login", templateVars);
+});
+router.get("/test", function(req, res, next) {
+	res.cookie("authorization", "Bearer " + security.makeToken(2, "deciare", lscrypto.hash("pass")));
+	res.render("index", { title: "LifeStream" });
 });
 
 module.exports = router;
