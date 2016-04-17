fe118b1e7e16b36df3c982501316e2ed96f8e385
Deciare <deciare@isisview.org>
2016-04-17 01:53:44 -0400

    Change models.error() to accept an optional non-string argument
    - msg should always be a string, sanitised for display to the user
    - detail may be anything, and should be logged by the application for debugging

--- routes/user.js
+++ routes/user.js
@@ -51,7 +51,7 @@ router.get("/new-token", function(req, res, next) {
 
 		dbmod.userGetById(tokenContents.id, function(err, row) {
 			if (err) {
-				return res.json(models.error(err));
+				return res.json("Server database error", models.error(err));
 			}
 
 			res.json(models.loginResponse(security.makeToken(tokenContents.id, row.login, row.pwhash)));
@@ -72,7 +72,7 @@ router.post("/login", function(req, res, next) {
 
 	dbmod.userLogin(login, pwhash, function(err, id) {
 		if (err)
-			res.json(models.error(err));
+			res.json("Server database error", models.error(err));
 		else {
 			var response = security.makeToken(id, login, pwhash);
 			res.json(models.loginResponse(response));
