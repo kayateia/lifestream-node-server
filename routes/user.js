c9ca1e35872f665a7837cad4691c54536a450a2a
Deciare <deciare@isisview.org>
2016-04-17 01:52:32 -0400

    Database user table changes
    - Add email and isadmin columns
    - Rename db.userGet() -> db.userGetById()
    - Add db.userGetByLogin()
    - db.userGetById() and db.userGetByLogin() calls should expect row object instead of a separate argument per column

--- routes/user.js
+++ routes/user.js
@@ -49,12 +49,12 @@ router.get("/new-token", function(req, res, next) {
 			return res.json(models.error(err));
 		}
 
-		dbmod.userGet(tokenContents.id, function(err, login, hash, name) {
+		dbmod.userGetById(tokenContents.id, function(err, row) {
 			if (err) {
 				return res.json(models.error(err));
 			}
 
-			res.json(models.loginResponse(security.makeToken(tokenContents.id, login, hash)));
+			res.json(models.loginResponse(security.makeToken(tokenContents.id, row.login, row.pwhash)));
 		});
 	});
 });
