91d9041bf66e3b92dc2db101b27a880e677f188a
Deciare <deciare@isisview.org>
2016-05-18 15:33:13 -0400

    Progressive loading in gallery
    - A placeholder exists at the bottom of an expanded gallery
    - When that placeholder is visible and scroll into the viewport, load another row of images
    - When the gallery is collapsed, unload all images that wouldn't fit on the first row
    - When the window is resized, load/unload images as needed to ensure that the gallery holds full rows of images

--- public/scripts/lifestream-app-gallery.js
+++ public/scripts/lifestream-app-gallery.js
@@ -1,7 +1,7 @@
-lsApp.controller("LifeStreamGalleryController", ["$scope", "$http", "lsKeepAlive", "lsSession", "$window", function($scope, $http, keepalive, session, $window) {
+lsApp.controller("LifeStreamGalleryController", ["$scope", "$element", "$http", "lsKeepAlive", "lsSession", "$timeout", "$window", function($scope, $element, $http, keepalive, session, $timeout, $window) {
 	var gallery = this;
 
-	gallery.numPerLoad = 10; // number of images to load at  time
+	gallery.numImagesPerRow = 0; // Number of images that would fit on each row in the gallery, taking into account the width of the grid, the size of each thumbnail, and the margin between each thumbnail
 	gallery.myStreams = [ 1 ]; // TODO: replace with array of stream IDs owned by user. Generally, the array should contain a single element specifying the user's auto-upload stream; otherwise, the same image may appear multiple times in the gallery if it's referenced from multiple streams
 	gallery.subscribedStreams = [ 1 ]; //TODO: replace with array of stream IDs to which user is subscribed
 	gallery.zoomed = false; // true after user clicks on in image in the grid
@@ -26,7 +26,10 @@ lsApp.controller("LifeStreamGalleryController", ["$scope", "$http", "lsKeepAlive
 		other: []
 	};
 
-	keepalive.begin();
+	// Shortcut for determining whether a given image ID already exists in an
+	// array.
+	gallery.images.mine.ids = [];
+	gallery.images.other.ids = [];
 
 	gallery.keepAlivePing = function() {
 		keepalive.ping();
@@ -36,11 +39,16 @@ lsApp.controller("LifeStreamGalleryController", ["$scope", "$http", "lsKeepAlive
 	//   arr - An array object under gallery.images, to store data from the
 	//     server.
 	//   streams - An array of stream IDs from which to load images.
-	//   fromTime - (optional) UNIX time. Only load images older than this.
-	gallery.loadImages = function(arr, streams, fromTime) {
+	//   count - (optional) Number of images to load.
+	//   olderThan - (optional) UNIX time. Only load images older than this.
+	//   callback - (optional) Function to be called when server response has
+	//      been successfully processed.
+	gallery.loadImages = function(arr, streams, count, olderThan, callback) {
 		streams.forEach(function(id) {
-			$http.get("api/stream/" +  id + "/contents?count=" + gallery.numPerLoad + (fromTime ? "&fromTime=" + fromTime : ""))
-				.then(
+			$http.get("api/stream/" +  id + "/contents?"
+				+ (count ? "&count=" + count : "")
+				+ (olderThan ? "&olderThan=" + olderThan : "")
+			).then(
 					function done(response) {
 						if (response.data.success) {
 							response.data.images.forEach(function(image) {
@@ -53,13 +61,16 @@ lsApp.controller("LifeStreamGalleryController", ["$scope", "$http", "lsKeepAlive
 									comment: image.comment
 								});
 
-								// Record this image's upload time. Since
-								// images are sent from the server in reverse
-								// chronological order, the current image is
-								// always the oldest image that has been
-								// received thus far
-								arr.oldestTime = image.uploadTime;
+								// Record this image's ID into a separate
+								// array, to make it quicker to identify
+								// whether a given image ID is already known
+								arr.ids.push(image.id);
 							});
+
+							// If a callback function was specified, call it
+							if (callback) {
+								callback();
+							}
 						}
 					},
 					function fail(response) {
@@ -69,10 +80,76 @@ lsApp.controller("LifeStreamGalleryController", ["$scope", "$http", "lsKeepAlive
 		});
 	};
 
+	// gallery.loadMoreImages()
+	//
+	//   Load one more row of images into an expanded gallery. This should be
+	//   called when the placeholder element at the bottom of the expanded
+	//   gallery is visible and its scroll position is within the viewport.
+	//
+	// Parameters:
+	//   arr - An array object under gallery.images
+	gallery.loadMoreImages = function(arr) {
+		// Only load more images if the gallery is expanded
+		if (!arr.expanded) {
+			return;
+		}
+
+		gallery.loadImages(arr, gallery.myStreams, gallery.numImagesPerRow, arr[arr.length - 1].uploadTime, function() {
+			// angular-inview only triggers the in-view check one time when
+			// the element enters or leaves the viewport.
+			//
+			// This means that even if the placeholder is still visible after
+			// loading more images, gallery.loadMoreImages() doesn't get
+			// called again because the placeholder won't be checked for
+			// being in-view again until the next scroll event.
+			//
+			// We work around this limitation by artificially generating a
+			// scroll event after a short delay. We cannot rely on
+			// $scope.$evalAsync() here because the delay is too short.
+			$timeout(function() {
+				angular.element(document).scroll();
+			}, 50);
+		});
+	}
+
+	// gallery.adjustImageRows()
+	//
+	//   Ensures that each row in the gallery grid contains a number of
+	//   thumbnails equal to what will fit on a single row.
+	//
+	// Parameters:
+	//   arr - An array object under gallery.images.
+	gallery.adjustImageRows = function(arr) {
+		// Check whether any blanks are left over on the last row of
+		// thumbnails.
+		var blanks = arr.length % gallery.numImagesPerRow;
+		if (blanks != 0) {
+			blanks = gallery.numImagesPerRow - blanks;
+		}
+
+		// If blanks are left over, and the gallery is expanded OR there
+		// aren't currently enough images to fill the first row, load
+		// more images to fill the blanks.
+		if (blanks > 0 && (arr.expanded || arr.length < gallery.numImagesPerRow)) {
+			gallery.loadImages(arr, gallery.myStreams, blanks, arr[arr.length - 1].uploadTime);
+		}
+
+		// If the gallery is collapsed, and the current number of
+		// displayed images won't fit in the new width, unload the images
+		// that won't fit.
+		if (!arr.expanded) {
+			for (var i = arr.length; i > gallery.numImagesPerRow; i--) {
+				arr.pop();
+			}
+		}
+
+	}
+
 	// Parameters:
 	//   arr - An array object under gallery.images.
 	gallery.collapseGrid = function(arr) {
 		arr.expanded = false;
+		gallery.adjustImageRows(arr);
 	};
 
 	// Parameters:
@@ -90,11 +167,39 @@ lsApp.controller("LifeStreamGalleryController", ["$scope", "$http", "lsKeepAlive
 		$window.location.href = arr[index].url;
 	};
 
-	// Load most recent images in streams on page load.
-	gallery.loadImages(gallery.images.mine, gallery.myStreams);
-	// Load user info on page load
-	session.queryUserInfo(userLogin);
+	// Watch the size of the gallery grid's element. We use this to calculate
+	// how many image thumbnails can fit on a single row.
+	//
+	// Each thumbnail is 196px = 192px image + 4px margin-right.
+	$scope.getElementDimensions = function() {
+		return { 'h': $element.height(), 'w': $element.width() };
+	};
+	$scope.$watch($scope.getElementDimensions, function(newValue, oldValue) {
+		gallery.numImagesPerRow = Math.floor(newValue.w / 196);
+	}, true);
+	angular.element(window).bind("resize", function() {
+		$scope.$apply();
+	});
 
+	// If the number of images that will fit on a row has changed, ensure
+	// that the number of images on each row matches the new width.
+	$scope.$watch("gallery.numImagesPerRow", function(newValue, oldValue) {
+		if (newValue != oldValue) {
+			gallery.adjustImageRows(gallery.images.mine);
+		}
+	});
+
+	// Functions to execute once the document is fully loaded.
+	angular.element(document).ready(function() {
+		// Begin keepalive timers
+		keepalive.begin();
+		// Load most recent images in streams on page load.
+		gallery.loadImages(gallery.images.mine, gallery.myStreams, gallery.numImagesPerRow);
+		// Load user info on page load
+		session.queryUserInfo(userLogin);
+	});
+
+	// Cancel keepalive timers when the app closes.
 	$scope.$on("$destroy", function() {
 		keepalive.end();
 	});
