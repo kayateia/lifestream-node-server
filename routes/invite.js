b67d2b8ab6ecc0814ab7db3708d07f2783eca200
Deciare <deciare@isisview.org>
2016-06-08 17:02:24 -0400

    Backend implementation of invite requests
    An invite request is a value in the state column of the subscription table.
    
    When a user requests an invite to a stream, the coresponding row in the subscription table has its state column set to dbconst.sub.requested.
    
    When a stream owner approes the invite request, the corresponding row in the subscription table has its state column set to dbconst.sub.invited.
    
    Finally, when a user accepts an invite, the corresponding row in the subscription table has its state column set to dbconst.sub.active.
    
    The following new APIs were implemented to facilitate the above functionality:
    - GET api/invite/:id/request
      - id: stream ID
      - get list of invite requests for stream
    - POST api/invite/:id/request
      - id: stream ID
      - userid (body): user ID
      - create invite request from user to stream
    - DELETE api/invite/:id/request
      - id: stream ID
      - userid (query): user ID
      - delete invite request from user to stream
    - GET api/subscription/:id/state
      - id: stream ID
      - userid (query): user ID
      - get state of user's subscription to stream (may be "active", "invited", "requested", or "none")

--- routes/invite.js
+++ routes/invite.js
@@ -9,8 +9,25 @@ var express = require("express");
 var router = express.Router();
 var models = require("./../lib/models");
 var dbmod = require("./../lib/db");
+var dbconst = require("./../lib/dbconst");
 var lscrypto = require("./../lib/lscrypto");
 var security = require("./../lib/security");
+
+// Get list of invite requests for the given stream
+router.get("/:id/request", function(req, res, next) {
+	security.validateLogin(req, res, function(err, tokenContents) {
+		if (err) {
+			return res.json(err);
+		}
+
+		dbmod.inviteRequestList(Number(req.params.id), function(err, invites) {
+			if (err) {
+				return res.json(err);
+			}
+			res.json(models.inviteRequestList(invites));
+		});
+	});
+});
 
 // Get list of invites for the given stream
 router.get("/:id", function(req, res, next) {
@@ -24,6 +41,38 @@ router.get("/:id", function(req, res, next) {
 				return res.json(err);
 			}
 			res.json(models.inviteList(invites));
+		});
+	});
+});
+
+// Create invite request for stream
+router.post("/:id/request", function(req, res, next) {
+	security.validateLogin(req, res, function(err, tokenContents) {
+		if (err) {
+			return res.json(err);
+		}
+
+		if (req.body.userid === undefined || Number(req.body.userid) < 1) {
+			return res.json(models.error("Invalid 'userid'"));
+		}
+
+		dbmod.streamInfo(req.params.id, function(err, stream) {
+			if (err) {
+				return res.json(err);
+			}
+
+			// Invites can only be requested for streams that need approval
+			if (stream.permission != dbconst.perms.approval) {
+				return res.json(models.error("Permission denied"));
+			}
+			else {
+				dbmod.inviteRequestCreate(Number(req.params.id), Number(req.body.userid), function(err) {
+					if (err) {
+						return res.json(err);
+					}
+					res.json(models.success());
+				});
+			}
 		});
 	});
 });
@@ -49,7 +98,44 @@ router.post("/:id", function(req, res, next) {
 				return res.json(models.error("Permission denied"));
 			}
 			else {
-				dbmod.inviteCreate(Number(req.params.id), Number(req.body.userid), function(err, invites) {
+				dbmod.inviteCreate(Number(req.params.id), Number(req.body.userid), function(err) {
+					if (err) {
+						return res.json(err);
+					}
+					res.json(models.success());
+				});
+			}
+		});
+	});
+});
+
+// Cancel invite request for stream
+router.delete("/:id/request", function(req, res, next) {
+	security.validateLogin(req, res, function(err, tokenContents) {
+		if (err) {
+			return res.json(err);
+		}
+
+		// Validate user ID
+		if (req.query.userid !== undefined) {
+			req.query.userid = Number(req.query.userid);
+		}
+		if (!req.query.userid || Number.isNaN(req.query.userid)) {
+			return res.json(models.error("Invalid 'userid'"));
+		}
+
+		dbmod.streamInfo(req.params.id, function(err, stream) {
+			if (err) {
+				return res.json(err);
+			}
+
+			// Only the owner of a stream can delete others' invite requests.
+			// Requestors can delete their own invite requests.
+			if (req.query.userid != tokenContents.id && stream.userid != tokenContents.id) {
+				return res.json(models.error("Permission denied"));
+			}
+			else {
+				dbmod.inviteRequestDelete(Number(req.params.id), Number(req.query.userid), function(err) {
 					if (err) {
 						return res.json(err);
 					}
--- routes/subscription.js
+++ routes/subscription.js
@@ -9,6 +9,7 @@ var express = require("express");
 var router = express.Router();
 var models = require("./../lib/models");
 var dbmod = require("./../lib/db");
+var dbconst = require("./../lib/dbconst");
 var lscrypto = require("./../lib/lscrypto");
 var security = require("./../lib/security");
 
@@ -24,6 +25,78 @@ router.get("/user/:id", function(req, res, next) {
 				return res.json(err);
 			}
 			res.json(models.subscriptionList(subscriptions));
+		});
+	});
+});
+
+// Get user's subscription state to one or more streams.
+// A state is always returned for each stream requested, even if no
+// corresponding row exists in the database subscription table.
+router.get("/:id/state", function(req, res, next) {
+	security.validateLogin(req, res, function(err, tokenContents) {
+		if (err) {
+			return res.json(err);
+		}
+
+		// Validate user ID
+		if (req.query.userid !== undefined) {
+			req.query.userid = Number(req.query.userid);
+		}
+		if (!req.query.userid || Number.isNaN(req.query.userid)) {
+			return res.json(models.error("Invalid 'userid'"));
+		}
+
+		// Sanitise comma-delimieted list by removing all non-numbers
+		var ids = [];
+		req.params.id.split(",").forEach(function(value, index, arr) {
+			value = Number(value);
+			if (!Number.isNaN(value)) {
+				ids.push(value);
+			}
+		});
+		var idsStr = ids.join(",");
+
+		dbmod.subscriptionState(idsStr, req.query.userid, function(err, states) {
+			if (err) {
+				return res.json(err);
+			}
+
+			// Process each row returned from the database
+			states.forEach(function(value, index, arr) {
+				// For each streamid in the response from the database, remove
+				// it from the array of streams that were originally requested
+				ids.splice(ids.indexOf(value.streamid), 1);
+
+				// Convert numeric states into words; discard unrecognised
+				// states
+				switch (value.state) {
+					case dbconst.sub.active:
+						arr[index].state = "active";
+						break;
+					case dbconst.sub.invited:
+						arr[index].state = "invited";
+						break;
+					case dbconst.sub.requested:
+						arr[index].state = "requested";
+						break;
+					default:
+						arr[index].state = "none";
+				}
+			});
+
+			// Any streams remaining in the array of streams that where
+			// originally requested, did not match any rows in the database.
+			// Append these streams to the database response with a state of
+			// "none".
+			ids.forEach(function(value) {
+				states.push({
+					streamid: value,
+					userid: req.query.userid,
+					state: "none"
+				});
+			});
+
+			res.json(models.subscriptionStates(states));
 		});
 	});
 });
