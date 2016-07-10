f75f412807b6c7836d125a26a0f1eb48d0549ae1
Kayateia <github@kayateia.net>
2016-07-10 15:18:27 -0700

    Some more push toward a working usermgr test

--- tests/usermgr_test.js
+++ tests/usermgr_test.js
@@ -1,17 +1,22 @@
 describe("UserListController", function() {
 	beforeEach(module("LifeStreamWebApp"));
 
-	var $controller;
+	var $controller, $rootScope;
 
-	beforeEach(inject(function(_$controller_) {
+	beforeEach(inject(function(_$controller_, _$rootScope_) {
 		$controller = _$controller_;
+		$rootScope = _$rootScope_;
 	}));
 
 	describe("formCtrl.loadUsers", function() {
 		it("gets a list of users from the server", function() {
-			var $scope = {};
+			var $scope = $rootScope.$new();
+			$scope.usermgr = $controller("LifeStreamUserManager", { $scope: $rootScope.$new() });
 			var formCtrl = {};
-			var controller = $controller("UserListController", { $scope: $scope, formCtrl: formCtrl });
+			var controller = $controller("UserListController", {
+				$scope: $scope,
+				formCtrl: formCtrl
+			});
 			formCtrl.loadUsers();
 			expect(formCtrl.users).not.toBe([]);
 		});
